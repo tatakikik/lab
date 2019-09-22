fd28f364546d6e8d4f48885048a790de1c144529
tatakikik <janjira_kikik@hotmail.com>
2019-09-22 21:29:08 +0700

    commit Final

--- src/App.js
+++ src/App.js
@@ -3,32 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 import CharacterCard from './CharacterCard';
 import WordCard from './WordCard';
+
 const word = "Hello";
-
-
 class App extends Component {
   render() {
-
     return (
       <div className="bg">
-      
         <div className="App">
-        <div className="text">
-    
-          
-        </div>
-        <div className="text">
-        
-        </div>
-          
-          <WordCard value="Hello" />
-          
-          
+          <WordCard value="Tata_kikik" />
+          <WordCard value="5935512079" />
         </div>
       </div>
       
     );
   }
 }
-
 export default App;
